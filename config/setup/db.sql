ddf13aea80aac2ad922a299e255655c633cc0216
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2024-09-06 09:46:15 +0600

    cleaup images and config/setup files

---
+++ config/setup/db.sql
@@ -0,0 +1,59 @@
+-- Drop Tables
+DROP TABLE users;
+DROP TABLE categories;
+DROP TABLE posts;
+DROP TABLE reviews;
+
+-- Create Tables
+CREATE TABLE users(
+  userId INT AUTO_INCREMENT UNIQUE,
+  firstName VARCHAR(50) NOT NULL,
+  lastName VARCHAR(50) NOT NULL,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  profileImage VARCHAR(255) DEFAULT "/img/user.jpg",
+  birthDate DATE,
+  country VARCHAR(50),
+  profession VARCHAR(50),
+  about VARCHAR(255),
+  isAdmin BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(userId)
+);
+
+CREATE TABLE categories(
+  categoryId INT AUTO_INCREMENT UNIQUE,
+  category VARCHAR(50) UNIQUE NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(categoryId)
+);
+
+CREATE TABLE posts(
+  postId INT AUTO_INCREMENT UNIQUE,
+  postTitle VARCHAR(255) NOT NULL,
+  postBody LONGTEXT NOT NULL,
+  postImage VARCHAR(255) NOT NULL,
+  postTags VARCHAR(255),
+  isFeatured BOOLEAN DEFAULT false,
+  postAuthorId INT NOT NULL,
+  postCategoryId INT,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(postId),
+  FOREIGN KEY(postAuthorId) REFERENCES users(userId) ON DELETE CASCADE,
+  FOREIGN KEY(postCategoryId) REFERENCES categories(categoryId) ON DELETE SET NULL
+);
+
+CREATE TABLE reviews(
+  reviewId INT AUTO_INCREMENT UNIQUE,
+  review VARCHAR(255) NOT NULL,
+  authorName VARCHAR(100) NOT NULL,
+  authorImage VARCHAR(255),
+  reviewAuthorId INT NOT NULL,
+  reviewPostId INT NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(reviewId),
+  FOREIGN KEY(reviewAuthorId) REFERENCES users(userId) ON DELETE CASCADE,
+  FOREIGN KEY(reviewPostId) REFERENCES posts(postId) ON DELETE CASCADE
+);
