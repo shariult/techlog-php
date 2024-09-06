4769e0064dd17075bbe3c34f86e830f6b6252aee
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2024-09-06 10:06:59 +0600

    change footer link icon color to white

--- App/Views/partials/footer.php
+++ App/Views/partials/footer.php
@@ -2,10 +2,11 @@
   <div class="container text-center">
     <img src="/img/logo-h.4bbf121b.png" class="img-fluid mb-4" width="192" alt="logo of tech log" />
     <div class="mb-2 text-center">
-      <a href="https://github.com/shariult" target="_blank" class="fs-4 me-2"><i
+      <a href="https://github.com/shariult" target="_blank" class="footer__link fs-4 me-2"><i
           class="fa-brands fa-github social__icon"></i></a>
-      <a href="#" target="_blank" class="fs-4 me-2"><i class="fa-brands fa-instagram social__icon"></i></a>
-      <a href="https://shariul.com" target="_blank" class="fs-4 me-2"><i class="fa-globe fa-solid social__icon"></i></a>
+      <a href="#" target="_blank" class="footer__link fs-4 me-2"><i class="fa-brands fa-instagram social__icon"></i></a>
+      <a href="https://shariul.com" target="_blank" class="footer__link fs-4 me-2"><i
+          class="fa-globe fa-solid social__icon"></i></a>
     </div>
     <p class="m-0 text-center text-white-50">
       Copyright &copy; 2024 Tech Log. All rights reserved. Developed by
