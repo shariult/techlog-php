741dd8df55b9d29c4dd72adcf31601281beeccbc
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2024-09-06 09:38:19 +0600

    re-organize routes

--- routes.php
+++ routes.php
@@ -1,19 +1,20 @@
 <?php
 
 $router->get("/", "IndexController@getIndex");
+
 $router->get("/login", "IndexController@getLogin", ["guest"]);
-$router->get("/logout", "IndexController@getLogout", ["auth"]);
+$router->post("/login", "IndexController@postLogin", ["guest"]);
 $router->get("/register", "IndexController@getRegister", ["guest"]);
+$router->post("/register", "IndexController@postRegister", ["guest"]);
+$router->get("/logout", "IndexController@getLogout", ["auth"]);
 
 $router->get("/blog", "BlogController@getBlogList");
-$router->get("/blog/create", "BlogController@getBlogCreate", ["auth"]);
 $router->get("/blog/{postId}", "BlogController@getBlogShow");
-$router->get("/blog/{postId}/edit", "BlogController@getBlogEdit", ["auth"]);
-
-$router->post("/login", "IndexController@postLogin", ["guest"]);
-$router->post("/register", "IndexController@postRegister", ["guest"]);
+$router->get("/blog/create", "BlogController@getBlogCreate", ["auth"]);
 $router->post("/blog", "BlogController@postBlog", ["auth"]);
-$router->post("/blog/{postId}/review", "ReviewController@postReview", ["auth"]);
+$router->get("/blog/{postId}/edit", "BlogController@getBlogEdit", ["auth"]);
 $router->put("/blog/{postId}", "BlogController@putBlog", ["auth"]);
+
+$router->post("/blog/{postId}/review", "ReviewController@postReview", ["auth"]);
 
 ?>
